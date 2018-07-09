a3b5513bee454e9ddcd7a558b5063a9346163c06
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-07-09 21:58:45 +0200

    Initial Commit

---
+++ Kontrollstrukturen-C3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kontrollstrukturen-C3
+//
+//  Created by Magnus Kruschwitz on 09.07.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
