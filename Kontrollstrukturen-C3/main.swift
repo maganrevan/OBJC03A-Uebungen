a4bbaccb43f8e86d1a5dd20303e8a6cc9dba3600
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-07-10 20:18:59 +0200

    init

--- Kontrollstrukturen-C3/main.swift
+++ Kontrollstrukturen-C3/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+// Aufgabe 2.1
+
+/*
+print("Bitte geben Sie eine Ganzzahl ein:")
+var iNumber1 = Int(readLine()!)!
+print("Bitte geben Sie eine weitere Ganzzahl ein:")
+var iNumber2 = Int(readLine()!)!
+
+if iNumber1 < iNumber2{
+    print("\(iNumber1) ist kleiner als \(iNumber2)")
+}
+else if iNumber1 > iNumber2{
+    print("\(iNumber1) ist größer als \(iNumber2)")
+}
+else{
+    print("Beide Zahlen sind gleich.")
+}
+*/
+
+// Aufgabe 2.2
+
+/*
+print("Wählen Sie bitte ein Essen aus: ")
+print("1 Jägerschnitzel mit Pommes")
+print("2 Currywurst mit Pommes")
+print("3 Bratwurst mit Brötchen")
+print("Geben Sie Ihre Auswahl ein: ", terminator:"")
+ 
+let auswahl = Int(readLine()!)!
+ 
+switch auswahl{
+    case 1:
+        print("Sie haben ein Jägerschnitzel mit Pommes gewählt.")
+    case 2:
+        print("Sie haben eine Currywurst mit Pommes gewählt.")
+    case 3:
+        print("Sie haben eine Bratwurst mit Brötchen gewählt.")
+    default:
+        print("Ihre Auswahl war nicht gültig.")
+}
+*/
+
 
